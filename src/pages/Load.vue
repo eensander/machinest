2c6d19953b4e5fe7abdf1cf83088d6d054e64df1
Sander (Portable) <>
2021-09-15 12:11:19 +0200

    starting point of stream reader, and stumbled across PapaParse

--- src/pages/Load.vue
+++ src/pages/Load.vue
@@ -4,7 +4,12 @@
 
     <p>In this window (...). </p>
 
-    <!-- <button @click="$router.push({name: 'load'})">(1) Start loading data</button> -->
+    <div>
+        <input type="file" @change="file_changed($event)">
+        <div>
+            <span class="bg-red-400">{{ filename }}</span>
+        </div>
+    </div>
 
     <div class="flex justify-between mt-8">
         <router-link :to="{ name: 'home' }" class=" bg-blue-600 px-4 py-1 text-white border-b-4 border-blue-400">
@@ -22,6 +27,32 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 export default defineComponent({
+
+	data: () => ({
+        filename: null
+	}),
+
+    methods: {
+        file_changed(event: any) {
+            console.log(event)
+            this.filename = event.target.files[0].name;
+            const file = event.target.files[0];
+
+            const reader = new FileReader();
+
+            console.log(reader.readAsArrayBuffer(file));
+
+            reader.onload = function(e) {
+                var arrayBuffer = reader.result;
+                console.log(arrayBuffer)
+
+                if (arrayBuffer != null)
+                    console.log(arrayBuffer.slice(0, 10))
+            }
+
+            // https://www.papaparse.com/
+        }
+    },
 
 });
 </script>
