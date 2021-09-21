cd144ad10713d14e513909787408ac81e2220455
eensander <sanderve@protonmail.com>
2021-09-21 10:11:39 +0200

    huh

--- src/composables/useConfig/index.ts
+++ src/composables/useConfig/index.ts
@@ -1,6 +1,7 @@
 import { Ref, ref } from "vue";
 
 import { TrainingMethod, TrainingMethodCategory } from '../types'
+import { ParseConfig } from 'papaparse'
 
 // import { default as useTrainingMethods } from '../useTrainingMethods'
 
@@ -9,6 +10,8 @@ interface UseConfig {
 		dataset: Ref<File | null>,
 		model: Ref<File | null>,
 	},
+
+	dataset_config: Ref<ParseConfig>
 
 	training_method_category: Ref<TrainingMethodCategory | null>,
 	training_method: Ref<TrainingMethod | null>,
@@ -37,6 +40,8 @@ const files = {
 	model: ref(null),
 }
 
+const dataset_config = ref({})
+
 const training_method_category = ref(null)
 const training_method = ref(null)
 
@@ -48,6 +53,8 @@ export default function useConfig(): UseConfig {
 	// return toRefs(state)
 	return {
 		files,
+
+		dataset_config,
 	
 		training_method_category,
 		training_method,
